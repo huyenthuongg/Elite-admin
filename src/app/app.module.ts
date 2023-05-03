ba6b2dc41f72faf0fbbcda34dc3af32ef7d3f291
NguyenHoangTinh <tinhnh20406c@st.uel.edu.vn>
2023-05-03 23:21:23 +0700

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ProductsComponent } from './products/products.component';
+import { BlogsComponent } from './blogs/blogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    ProductsComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
