733d4fd02061344f9afc9e11d0a713d036d35c86
NguyenHoangTinh <tinhnh20406c@st.uel.edu.vn>
2023-05-03 23:23:39 +0700

    admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountsComponent } from './accounts/accounts.component';
-import { ProductsComponent } from './products/products.component';
-import { BlogsComponent } from './blogs/blogs.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
 
 @NgModule({
   declarations: [
